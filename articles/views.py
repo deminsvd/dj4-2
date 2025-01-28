7ca2ba35d45fc07da430fc4667e67d0a2301134b
DS test <demin.svd@yandex.ru>
2025-01-28 17:29:21 +0300

    dj4-2-fin

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
-from articles.models import Article
+from .models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    articles_db = Article.objects.all().order_by('-published_at')
+    context = {
+        'object_list': articles_db
+    }
+
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
--- website/urls.py
+++ website/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from articles.views import articles_list
 
 urlpatterns = [
-    path('', include('articles.urls')),
+    # path('', include('articles.urls')),
+    path('', articles_list, name='articles'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
